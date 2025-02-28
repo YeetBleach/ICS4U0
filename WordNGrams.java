275003c9f3518039f69d077787cf6b1e3add0cfc
YeetBleach <156724136+YeetBleach@users.noreply.github.com>
2025-02-28 10:10:15 -0500

    finished but can improve efficiency

--- WordNGrams.java
+++ WordNGrams.java
@@ -11,18 +11,63 @@ public class WordNGrams {
         //character trigram
         ArrayList<String> charTrigramsTemp = new ArrayList<>(Arrays.asList(sentence.split("")));
         ArrayList<String> charTrigrams = new ArrayList<>();
-        String temp = "";
-        while (!charTrigramsTemp.isEmpty()){
-            for (int i = 0; i < 3; i++){
-                if (charTrigramsTemp.size() < 3){
-                    break;
-                }
-                temp += charTrigramsTemp.get(i); // spacing cases
+        StringBuilder temp = new StringBuilder();
+        while (!charTrigramsTemp.isEmpty()) {
+            if (charTrigramsTemp.size() < 3) {
+                break; // Stop when fewer than 3 characters remain
             }
+            for (int i = 0; i < 3; i++) {
+                temp.append(charTrigramsTemp.get(i));
+            }
+            charTrigrams.add(temp.toString()); // Add only complete trigrams
             charTrigramsTemp.remove(0);
-            charTrigrams.add(temp);
-            temp = "";
+            temp = new StringBuilder();
         }
         System.out.println("Character trigrams: " + charTrigrams);
+
+        //word bigram
+        ArrayList<String> wordBigramsTemp = new ArrayList<>(Arrays.asList(sentence.split(" ")));
+        ArrayList<String> wordBigrams = new ArrayList<>();
+        StringBuilder temp2 = new StringBuilder();
+        if (wordBigramsTemp.size() >= 2) {
+            while (!wordBigramsTemp.isEmpty()) {
+                if (wordBigramsTemp.size() < 2) {
+                    break; // Stop when fewer than 2 words remain
+                }
+                for (int i = 0; i < 2; i++) {
+                    temp2.append(wordBigramsTemp.get(i)).append(" ");
+                }
+                wordBigrams.add(temp2.toString().stripTrailing()); // Add only complete bigrams
+                wordBigramsTemp.remove(0);
+                temp2 = new StringBuilder();
+            }
+            System.out.println("Word bigrams: " + wordBigrams);
+        }
+        else{
+            System.out.println("Word bigrams: Not enough words for bigram");
+        }
+
+        //word trigram
+        ArrayList<String> wordTrigramsTemp = new ArrayList<>(Arrays.asList(sentence.split(" ")));
+        ArrayList<String> wordTrigrams = new ArrayList<>();
+        StringBuilder temp3 = new StringBuilder();
+        if (wordTrigramsTemp.size() >= 3) {
+            while (!wordTrigramsTemp.isEmpty()) {
+                if (wordTrigramsTemp.size() < 3) {;
+                    break; // Stop when fewer than 3 words remain
+                }
+                for (int i = 0; i < 3; i++) {
+                    temp3.append(wordTrigramsTemp.get(i)).append(" ");
+                }
+                wordTrigrams.add(temp3.toString().stripTrailing()); // Add only complete trigrams
+                wordTrigramsTemp.remove(0);
+                temp3 = new StringBuilder();
+            }
+            System.out.println("Word trigrams: " + wordTrigrams);
+        }
+        else{
+            System.out.println("Word trigrams: Not enough words for trigram");
+        }
+
     }
 }
