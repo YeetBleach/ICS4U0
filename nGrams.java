afcd115e940bc5f315237f12ff1d467deda55381
YeetBleach <156724136+YeetBleach@users.noreply.github.com>
2025-03-03 00:45:56 -0500

    Update nGrams.java

--- nGrams.java
+++ nGrams.java
@@ -1,34 +1,24 @@
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class nGrams {
-    public static int n;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a word: ");
         String word = scanner.nextLine();
-        boolean invalid = true;
+
+        int n;
         System.out.println("What is your n value?: ");
-        while (invalid) {
+        while (true) {
             n = scanner.nextInt();
-            if (n>word.length()){
-                System.out.println("n cannot more than the number of words");
+            if (n > word.length()) {
+                System.out.println("n cannot be more than the length of the word");
                 System.out.println("What is your n value? ");
-            }
-            else{
-                invalid = false;
+            } else {
+                break;
             }
         }
-        ArrayList<String> charArr = new ArrayList<>(Arrays.asList(word.split("")));
-        while (!charArr.isEmpty()){
-            for (int i = 0; i < n; i++){
-                if (charArr.size() < n){ break;}
-                System.out.print(charArr.get(i));
-            }
-            charArr.remove(0);
-            System.out.println();
-
+        for (int i = 0; i <= word.length() - n; i++) {
+            System.out.println(word.substring(i, i + n));
         }
     }
 }
